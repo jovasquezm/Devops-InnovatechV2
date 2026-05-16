f36be8290411cc393e4f1b2ef33e3cb5d3cd4c35
olovj222 <ol.juengt@duocuc.cl>
2026-05-15 22:44:19 -0400

    hola

--- infra/main.tf
+++ infra/main.tf
@@ -390,6 +390,14 @@ resource "aws_ecs_task_definition" "app" {
       portMappings = [
         { containerPort = 8080 }
       ]
+       healthCheck = {
+          command     = ["CMD-SHELL", "wget -qO- http://localhost:8080 || exit 1"]
+          interval    = 30
+          timeout     = 5
+          retries     = 3
+          startPeriod = 30
+        }
+
 
       dependsOn = [
         { containerName = "ms-ventas",    condition = "START" },
